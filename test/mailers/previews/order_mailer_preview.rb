707b2b4bd39bf06628951c4dfabf210ccd1e14ba
bragamat <mateuscbraga@gmail.com>
2018-04-28 21:11:06 +0000

    feature mailer working

---
+++ test/mailers/previews/order_mailer_preview.rb
@@ -0,0 +1,12 @@
+class OrderMailerPreview < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.new_order.subject
+  #
+  def new_order
+   order = Order.first
+   OrderMailer.new_order(order)
+  end
+end
