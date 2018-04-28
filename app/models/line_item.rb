e5fb670b04b057c2875681d79c40fdfc25728b4e
bragamat <mateuscbraga@gmail.com>
2018-04-28 18:32:13 +0000

    cart bug fixed

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,4 +6,19 @@ class LineItem < ActiveRecord::Base
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
 
+  # def allItems
+  #   @order_items = findingItem(LineItem.all)
+  #   @order_items
+  # end
+
+
+
+# private
+  
+#   def fidingItem(arr)
+#     arr.each do |item|
+#       item.order_id
+#     end
+#   end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,5 +6,23 @@ class Order < ActiveRecord::Base
   monetize :total_cents, numericality: true
 
   validates :stripe_charge_id, presence: true
+  
+
+  def allItems 
+  findProducts 
+    prodName = []
+     Order.first.line_items.each do |line|
+      prodName << line.product
+     end    
+  end
+
+private
+
+  def findProducts
+    prod = []
+     Order.first.line_items.each do |line|
+      prod << line.product
+     end    
+  end
 
 end
